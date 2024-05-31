416d4ffe5b3c8cb22e795726201ff81bcc7427b1
david huveau <davidhuveau@macbook-pro-davidnew.home>
2024-05-31 22:07:07 +0200

    adding eslint & prettier configuration

--- src/add.test.ts
+++ src/add.test.ts
@@ -1,8 +1,8 @@
 import { add } from "./add";
-import { describe, expect, test } from '@jest/globals';
+import { describe, expect, test } from "@jest/globals";
 
-describe('sum module', () => {
-  test('adds 1 + 2 to equal 3', () => {
+describe("sum module", () => {
+  test("adds 1 + 2 to equal 3", () => {
     expect(add(1, 2)).toBe(3);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import { add } from "./add";
+
 console.log("hello");
 console.log(add(1, 2));
